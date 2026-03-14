c8a0f3a8e3731b625043052afa42ce7e8f06754f
Archana200606 <archanareddyk02@gmail.com>
2026-03-14 19:25:03 +0530

    change

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://trustchain-backend-12kl.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
